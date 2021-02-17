ceb6a88d41d7758bfa84ce34a97d4d2f26956f32
Anaya <73314637+Anaya324@users.noreply.github.com>
2021-02-17 16:54:15 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,50 @@
 name_of_the_student_array = [];
 
-function submit() 
-{
-    var name_1 = document.getElementById("Name_Of_Student_1").value;
-    var name_2 = document.getElementById("Name_Of_Student_2").value;
-    var name_3 = document.getElementById("Name_Of_Student_3").value;
-    var name_4 = document.getElementById("Name_Of_Student_4").value;
-    var name_5 = document.getElementById("Name_Of_Student_5").value;
+function submit() {
+    var display_student_array = [];
 
-    name_of_the_student_array.push(name_1);
-    name_of_the_student_array.push(name_2);
-    name_of_the_student_array.push(name_3);
-    name_of_the_student_array.push(name_4);
-    name_of_the_student_array.push(name_5);
+    for (var j = 1; j <= 4; j++) {
+        var name_of_the_student = document.getElementById("name_of_the_student_" + j).value;
+        console.log(name_of_the_student);
+        name_of_the_student_array.push(name_of_the_student);
+    }
 
     console.log(name_of_the_student_array);
 
-    document.getElementById("display_name").innerHTML = name_of_the_student_array;
-    document.getElementById("btnSubmit").style.display = "none";
-    document.getElementById("btnSort").style.display = "inline-block";
+    var lenght_of_name_of_students_array = name_of_the_student_array.length;
+    console.log(lenght_of_name_of_students_array);
+
+    for (var k = 0; k < lenght_of_name_of_students_array; k++) {
+        display_student_array.push("<h4>NAME - " + name_of_the_student_array[k] + "</h4>");
+        console.log(display_student_array);
+    }
+
+    console.log(display_student_array);
+    document.getElementById("display_name_with_commas").innerHTML = display_student_array;
+    var remove_commas = display_student_array.join(" ");
+    console.log(remove_commas);
+    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
+
+    document.getElementById("submit_button").style.display = "none";
+    document.getElementById("sort_button").style.display = "inline-block";
+
 }
-function sorting() 
-{
+
+function sort() {
     name_of_the_student_array.sort();
     console.log(name_of_the_student_array);
-    document.getElementById("display_name").innerHTML = name_of_the_student_array;
+
+    var display_student_array_sorting = [];
+
+    var lenght_of_name_of_students_array = name_of_the_student_array.length;
+    console.log(lenght_of_name_of_students_array);
+
+    for (var k = 0; k < lenght_of_name_of_students_array; k++) {
+        display_student_array_sorting.push("<h4>NAME - " + name_of_the_student_array[k] + "</h4>");
+        console.log(display_student_array_sorting);
+    }
+    var remove_commas = display_student_array_sorting.join(" ");
+    console.log(remove_commas)
+
+    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
 }
